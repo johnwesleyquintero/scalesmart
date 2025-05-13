e29ba6057a45f53251eaf2a354a3f0b436b6eff2
John Wesley Quintero <johnwesleyquintero@gmail.com>
2025-05-14 01:49:11 +0800

    fix blog header

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,5 +1,3 @@
-import Footer from '@/components/footer';
-import Header from '@/components/header';
 import type { Metadata, Viewport } from 'next';
 import React from 'react';
 
@@ -23,9 +21,7 @@ export default function BlogLayout({
 }) {
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-50 via-white to-blue-50 dark:from-gray-900 dark:via-gray-900 dark:to-gray-800">
-      <Header />
       <main>{children}</main>
-      <Footer />
     </div>
   );
 }
