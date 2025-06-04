5cd9f46792454088f90747150a090f068f4c578c
John Wesley Quintero <johnwesleyquintero@gmail.com>
2025-06-04 10:46:23 +0800

    feat(docs): add getting started page and layout components
    Add initial documentation structure with getting started page and layout components. This provides the foundation for the documentation section of the application.

---
+++ src/app/docs/getting-started/layout.tsx
@@ -0,0 +1,15 @@
+export default function GettingStartedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section>
+      {/* Include shared UI here, like a sidebar for docs navigation */}
+      <nav className="p-4 border-r">
+        {/* Example: <ul><li><a href="/docs/getting-started">Getting Started</a></li></ul> */}
+      </nav>
+      <main>{children}</main>
+    </section>
+  );
+}
