34d2c4177c4458d3a8c7924e3e6b51005d8ccd0b
John Wesley Quintero <johnwesleyquintero@gmail.com>
2025-07-06 12:22:23 +0800

    refactor(auth): remove redundant sign-in api endpoint
    The endpoint was redundant as it only redirected to the auth signin page, which can be handled directly by the client.

--- src/pages/api/sign-in.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  res.redirect('/api/auth/signin?provider=github');
-}
